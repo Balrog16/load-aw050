a1c93f1938218628abd978b0622a1852084d0d0d
Surya <surya.b@gmail.com>
2023-05-06 06:43:00 +0000

    StairCase Gen

--- main.cpp
+++ main.cpp
@@ -10,18 +10,26 @@
 Ticker flipper;
 DigitalOut led(LED1);
 AnalogOut aout(PA_4);
+float i = 0;
 void flip() { led = !led; }
+
+void testDAC()
+{
+  /*aout = 0.3;
+  i = i + 0.1;
+  if(i == 1.0f) i=0;*/
+}
 
 int main() {
   // Initialise the digital pin LED1 as an output
 
-  flipper.attach(&flip, 2s);
+  flipper.attach(&testDAC, 2s);
 
   while (true) {
 
     for (float i = 0.0f; i < 1.0f; i += 0.1f) {
       aout = i;
-      ThisThread::sleep_for(BLINKING_RATE);
-    }
+      ThisThread::sleep_for(BLINKING_RATE);}
+
   }
 }
