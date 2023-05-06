1b5bd171e665ac4c7bbbd6991b7ac6287c28b39a
Surya <surya.b@gmail.com>
2023-05-06 09:05:16 +0000

    Made line piecewise linear

--- main.cpp
+++ main.cpp
@@ -6,34 +6,63 @@
 #include "mbed.h"
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE 1500ms
+#define BLINKING_RATE 800ms
 Ticker flipper;
 DigitalOut led(LED1);
 AnalogOut aout(PA_4);
-DigitalOut led1(PB_4);
 
-float i = 0;
 void flip() { led = !led; }
 
-void testDAC()
-{
-    AnalogOut aout(PA_4);
 
-  aout = 0.3;
-  i = i + 0.1;
-  if(i == 1.0f) i=0;
-  led1 = !led1;
+void setCurrentmA(float fVal)
+{
+    //if fVal is zero .. put it to Sleep
+
+    float m=0, c=0;
+
+    if(fVal>0 && fVal<0.35) fVal = 0.35;
+    if(fVal>2.7 && fVal<3) fVal = 3;
+    if(fVal>5.13 && fVal<=5.3) fVal = 5.3;
+    if(fVal>=0.35 && fVal<=2.7)
+    {
+        m = 13.3839;
+        c = 0.0199;
+    }
+    else
+    {
+        if(fVal>=3 && fVal<=5.13)
+        {
+            m = 13.9429;
+            c = -0.1043;
+        }
+
+        if(fVal>=5.3 && fVal<=9.2)
+        {
+            m = 12.7761;
+            c = 0.1733;
+        }
+        if(fVal>9.2 && fVal<=12)
+        {
+            m = 12.3;
+            c = 0.55;
+        }
+    }
+
+    float setmA = (fVal - c)/m;
+    aout = setmA;
 }
+
+
+
 
 int main() {
   // Initialise the digital pin LED1 as an output
 
-  flipper.attach(&testDAC, 2s);
 
   while (true) {
 
-    for (float i = 0.0f; i < 1.0f; i += 0.1f) {
-      aout = i;
+    for (float i = 0.3f; i < 12.0f; i += 0.5f) {
+      setCurrentmA(i);
       ThisThread::sleep_for(BLINKING_RATE);}
 
   }
