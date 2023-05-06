d438c4bf887fa3a94ed1753d10eead03b7c9d36b
Surya <surya.b@gmail.com>
2023-05-06 06:15:29 +0000

    add timer

--- main.cpp
+++ main.cpp
@@ -8,19 +8,23 @@
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
+Ticker flipper;
+DigitalOut led(LED1);
 
+void flip()
+{
+    led = !led;
+}
 
 int main()
 {
     // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
+   
+
+    flipper.attach(&flip, 2s);
 
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        
+       // ThisThread::sleep_for(BLINKING_RATE);
     }
 }
